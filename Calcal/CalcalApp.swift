8f5a85cb92745226a9f7b28ea3b51527adf652bb
Iaroslav Erokhin <iaroslav.erokhin@check24.de>
2024-04-14 13:00:13 +0200

    Initial Commit

---
+++ Calcal/CalcalApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalcalApp.swift
+//  Calcal
+//
+//  Created by Iaroslav Erokhin on 14.04.24.
+//
+
+import SwiftUI
+
+@main
+struct CalcalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Calcal/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Calcal
+//
+//  Created by Iaroslav Erokhin on 14.04.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
